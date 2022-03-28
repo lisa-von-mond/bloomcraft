b5569852f1c9494e0e48c581d8ea0734d67e3f3a
Lisa Lu <lisalue@posteo.de>
2022-03-28 22:14:56 +0200

    write new moving functions

--- components/field.jsx
+++ components/field.jsx
@@ -7,8 +7,7 @@ export function Field(){
 const [galaxy, setGalaxy] = useState(levelOne)
 
 function toRadians(degrees) {
-    return degrees * (Math.PI / 180);
-}
+    return degrees * (Math.PI / 180);}
 
 function angleToCooX(angle, distance){
   const X = Math.cos(toRadians(angle)) * distance
@@ -18,115 +17,127 @@ function angleToCooY(angle, distance){
     const Y = Math.sin(toRadians(angle)) * distance             
     return parseInt(Y)}
 
+function IntegrateScope(thisBase, nextBase, nextBaseNew) {
+  const newScope = thisBase.children.map((element)=>(element === nextBase ? nextBaseNew : element))
+  const newBase = {...thisBase, children:newScope}
+  return newBase}
 
+// operation functions
+      
+function Beam1(myFunction, actualBase){
+  setGalaxy([myFunction(actualBase)])}
+      
+function Beam2(myFunction, actualBase){
+  const baseOne = trackedChild(actualBase)
+  const newBaseOne = myFunction(baseOne)
+  console.log(newBaseOne)
+  setGalaxy([IntegrateScope(actualBase, baseOne, newBaseOne)])}
+      
+function Beam3(myFunction, actualBase){
+  const baseOne = trackedChild(actualBase)
+  const baseTwo = trackedChild(baseOne)
+  const newBaseTwo = myFunction(baseTwo)
+  const newBaseOne = IntegrateScope(baseOne,baseTwo, newBaseTwo)
+  setGalaxy([IntegrateScope(actualBase, baseOne, newBaseOne)])} 
+      
+// navigation functions
+  
+function trackedChild(objekt){
+  const thisone = objekt.children.find((element)=>(element.tracked === true))
+  return thisone}
 
-    function turnRight(){
+// moving functions
 
-      const Base = galaxy[0] /// Objekt
-      
-      function turnFocusRightInScope(objekt){
-        const scopeX = objekt.children
-        const Focus = scopeX.find((element)=>(element.focus === true))
-        const nextFocusIndex = Focus.flow === 1 ? scopeX.length : Focus.flow - 1
-        const scopeXA = scopeX.map ((element)=>(element.focus === true ? {...element, focus:false} : element))
-        const scopeXNew = scopeXA.map ((element) => (element.flow === nextFocusIndex ? {...element, focus:true} : element))
-        return {...objekt, children:scopeXNew}
-    } 
-      
-       // bekommt ein Objekt als Argument, gibt Objekt mit verändertem Children-Array aus
-      
-      if (Base.active === true){
-      
-      const newGalaxy = [turnFocusRightInScope(Base)]
-      setGalaxy(newGalaxy)
-      
-      } else {
-      
-      const subBaseOne = Base.children.find((element)=>(element.active === true)) // Objekt
-      const newSubBaseOne = turnFocusRightInScope(subBaseOne)
-      const NewScopeOne = Base.children.map((element)=>(element === subBaseOne ? newSubBaseOne : element))
-      const NewBase = {...Base, children: NewScopeOne}
-      setGalaxy([NewBase]) }
-      
-  }
+function hopUpNow(){
+  let Base = galaxy[0]
+  if (Base.active === true){Beam1(hopUpInScope, Base)} else {
+  if (trackedChild(Base).active === true){Beam2(hopUpInScope, Base)} else {
+  if (trackedChild(trackedChild(Base)).active === true){Beam3(hopUpInScope, Base)}  
+  else {console.log("nothing to go up here")}
+ }}}
+
+ function turnFocusLeftNow(){
+  let Base = galaxy[0]
+  if (Base.active === true){Beam1(turnFocusLeftInScope, Base)} else {
+  if (trackedChild(Base).active === true){Beam2(turnFocusLeftInScope, Base)} else {
+  if (trackedChild(trackedChild(Base)).active === true){Beam3(turnFocusLeftInScope, Base)}  
+  else {console.log("nothing to turn left here")}
+ }}}
+
+ function turnFocusRightNow(){
+  let Base = galaxy[0]
+  if (Base.active === true){Beam1(turnFocusRightInScope, Base)} else {
+  if (trackedChild(Base).active === true){Beam2(turnFocusRightInScope, Base)} else {
+  if (trackedChild(trackedChild(Base)).active === true){Beam3(turnFocusRightInScope, Base)}  
+  else {console.log("nothing to turn left here")}
+ }}}
+
+function hopDownNow(){
+  let Base = galaxy[0]
+  if (Base.active === true){
+  console.log("nothing to go down here")} else {
+  if (trackedChild(Base).active === true){Beam1(hopDownInScope, Base)} else {
+  if (trackedChild(trackedChild(Base)).active === true){Beam2(hopDownInScope, Base)} else 
+  {Beam3(hopDownInScope, Base)}
+  }}}
+
+function hopUpInScope(object){
   
-  function turnLeft(){
-  
-  const Base = galaxy[0] /// Objekt
-  
-  function turnFocusLeftInScope(objekt){
+  const Focus = object.children.find((element) => (element.focus === true))
+  const NewSubChildren = Focus.children.map((element) => (element.flow === 1 ? {...element, focus: true} : element))
+  const newChildren = object.children.map((element) => (element.focus === true ? {...element, focus: false, active: true, tracked:true, children: NewSubChildren} : element))
+  const newObject = {...object, children: newChildren, active:false}
+  return newObject
+}
+
+function turnFocusLeftInScope(objekt){
+  const scopeX = objekt.children
+  const Focus = scopeX.find((element)=>(element.focus === true))
+  const nextFocusIndex = Focus.flow === scopeX.length ? 1 : Focus.flow + 1
+  const scopeXA = scopeX.map ((element)=>(element.focus === true ? {...element, focus:false} : element))
+  const scopeXNew = scopeXA.map ((element) => (element.flow === nextFocusIndex ? {...element, focus:true} : element))
+  return {...objekt, children:scopeXNew}}
+
+function turnFocusRightInScope(objekt){
     const scopeX = objekt.children
     const Focus = scopeX.find((element)=>(element.focus === true))
-    const nextFocusIndex = Focus.flow === scopeX.length ? 1 : Focus.flow + 1
+    const nextFocusIndex = Focus.flow === 1 ? scopeX.length : Focus.flow - 1
     const scopeXA = scopeX.map ((element)=>(element.focus === true ? {...element, focus:false} : element))
     const scopeXNew = scopeXA.map ((element) => (element.flow === nextFocusIndex ? {...element, focus:true} : element))
     return {...objekt, children:scopeXNew}}
-  
-  if (Base.active === true){
-  
-  const newGalaxy = [turnFocusLeftInScope(Base)]
-  setGalaxy(newGalaxy)
-  
-  } else {
-  
-  const subBaseOne = Base.children.find((element)=>(element.active === true)) // Objekt
-  const newSubBaseOne = turnFocusLeftInScope(subBaseOne)
-  const NewScopeOne = Base.children.map((element)=>(element === subBaseOne ? newSubBaseOne : element))
-  const NewBase = {...Base, children: NewScopeOne}
-  setGalaxy([NewBase])}
-  
-  }
-  
-  function upNow(){
-  
-  const Base = galaxy[0] /// Objekt
-  
-  function hopUpInScope(object){
-  
-    const Focus = object.children.find((element) => (element.focus === true))
-    const NewSubChildren = Focus.children.map((element) => (element.flow === 1 ? {...element, focus: true} : element))
-    const newChildren = object.children.map((element) => (element.focus === true ? {...element, focus: false, active: true, children: NewSubChildren} : element))
-    const newObject = {...object, children: newChildren, active:false}
-    return newObject
-  }
-  
-  if (Base.active === true){
-  
-    const newGalaxy = [hopUpInScope(Base)]
-    setGalaxy(newGalaxy)
-    
-    } else {
-    
-    const subBase = Base.children.find((element)=>(element.active === true)) // Objekt
-    const newSubBase = hopUpInScope(subBase)
-    const NewScopeOne = Base.children.map((element)=>(element === subBase ? newSubBase : element))
-    const NewBase = {...Base, children: NewScopeOne}
-    setGalaxy([NewBase])}
-  }
 
+
+function hopDownInScope(object){
+
+const Current = object.children.find((element) => (element.active === true))
+const NewSubChildren = Current.children.map((element) => ({...element, focus: false}))
+const newChildren = object.children.map((element) => (element === Current? {...element, focus: true, active: false, tracked: false, children: NewSubChildren} : element))
+const newObject = {...object, children: newChildren, active:true}
+return newObject
+       }
+
+
+  
 return(
 <>
-{galaxy.map((one)=><MyGalaxy key={one.name} distancev={one.distv} type={one.type} active={one.active} focus={one.focus}>
-    {one.children.map((two)=><MyGalaxy key={two.name} distx={angleToCooX(two.angl, two.dist)} disty={angleToCooY(two.angl, two.dist)} type={two.type} active={two.active} focus={two.focus}>
-  
-    {two.children.map((three)=><MyGalaxy key={three.name} distx={angleToCooX(three.angl, three.dist)} disty={angleToCooY(three.angl, three.dist)} type={three.type} active={three.active} focus={three.focus}>
-  
-    </MyGalaxy>)}
+{galaxy.map((one)=><MyGalaxy1 key={one.name} distancev={one.distv} type={one.type} flow={one.flow} active={one.active} focus={one.focus}>
+    {one.children.map((two)=><MyGalaxy2 key={two.name} distx={angleToCooX(two.angl, two.dist)} disty={angleToCooY(two.angl, two.dist)} flow={two.flow} type={two.type} active={two.active} focus={two.focus}>
+      {two.children.map((three)=><MyGalaxy3 key={three.name} distx={angleToCooX(three.angl, three.dist)} disty={angleToCooY(three.angl, three.dist)} flow={three.flow} type={three.type} active={three.active} focus={three.focus}>
+      {three.children.map((four)=><MyGalaxy4 key={four.name} distx={angleToCooX(four.angl, four.dist)} disty={angleToCooY(four.angl, four.dist)} type={four.type} flow={four.flow} active={four.active} focus={four.focus}>
+                  </MyGalaxy4>)}
+                  </MyGalaxy3>)}
+                  </MyGalaxy2>)}
+                  </MyGalaxy1>)}
 
-    </MyGalaxy>)}
-  </MyGalaxy>)}
-
-<TestButton1 onClick = {upNow}>UP</TestButton1>
-<TestButton2>(DOWN)</TestButton2>
-<TestButton3 onClick = {turnLeft}>LEFT</TestButton3>
-<TestButton4 onClick = {turnRight}>RIGHT</TestButton4>
+<TestButton1 onClick = {hopUpNow}>UP</TestButton1>
+<TestButton2 onClick = {hopDownNow}>DOWN</TestButton2>
+<TestButton3 onClick = {turnFocusLeftNow}>LEFT</TestButton3>
+<TestButton4 onClick = {turnFocusRightNow}>RIGHT</TestButton4>
 </>
 )
-
 }
 
-
-const MyGalaxy = styled.div`
+const MyGalaxy1 = styled.div`
 top:${props => props.distx}px;
 left:${props => props.disty}px;
 
@@ -135,51 +146,111 @@ position:absolute;
 display:flex;
 justify-content:center;
 align-items:center;
+background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+height: 100px;
+width: 100px;
+border-radius:50%;
+box-shadow: 0.2em 0.2em 2em white;
 
-${(props) => props.focus === true &&
-css`
-
-`}
-
-
-  ${(props) => props.type === "planet" &&
-  css`
-  background-image: linear-gradient(to top, #5ee7df 0%, #b490ca 100%);
-    height: 70px;
-    width: 70px;
-    border-radius:50%;
-    transform:translate(15px,15px);
-  `}
-
-  ${(props) => props.type === "moon" &&
-  css`
-
-  background-image: linear-gradient(to top, #ebbba7 0%, #cfc7f8 100%);
-    height: 50px;
-    width: 50px;
-    border-radius:50%;
-    transform:translate(10px,10px);
-  `}
-
-  ${(props) => props.type === "sun" &&
-  css`
-
-  background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
-  height: 100px;
-  width: 100px;
-  border-radius:50%;`}
-
-  ${(props) => props.active === true &&
+${(props) => props.active === true &&
     css`
     box-shadow: 0.2em 0.2em 2em 0.2em gold;`}
 
+${(props) => props.focus === true &&
+    css`
+    box-shadow: 0.3em 0.3em 3em 0.2em hotpink;`}
+`
+
+const MyGalaxy2 = styled.div`
+top:${props => props.distx}px;
+left:${props => props.disty}px;
+      
+text-align:center;
+position:absolute;
+display:flex;
+justify-content:center;
+align-items:center;
+background-image: linear-gradient(to top, #5ee7df 0%, #b490ca 100%);
+height: 70px;
+width: 70px;
+border-radius:50%;
+transform:translate(15px,15px);
+   
+  ${(props) => props.active === true &&
+    css`
+    box-shadow: 0.2em 0.2em 2em 0.2em gold;
+    border:2px dotted gold;
+    `}
+      
   ${(props) => props.focus === true &&
+    css`
+    box-shadow: 0.3em 0.3em 3em 0.2em hotpink;`}
+  
+  ${(props) => props.flow === 1 &&
+    css`
+    border:2px dotted white;`}
+`
+
+const MyGalaxy3 = styled.div`
+top:${props => props.distx}px;
+left:${props => props.disty}px;
+      
+text-align:center;
+position:absolute;
+display:flex;
+justify-content:center;
+align-items:center;
+background-image: linear-gradient(to top, #ebbba7 0%, #cfc7f8 100%);
+height: 70px;
+width: 70px;
+border-radius:50%;
+transform:translate(15px,15px);
+      
+${(props) => props.active === true &&
+    css`
+    box-shadow: 0.2em 0.2em 2em 0.2em gold;
+    border:2px dotted gold;`}
+      
+${(props) => props.focus === true &&
+    css`
+    box-shadow: 0.3em 0.3em 3em 0.2em hotpink;`}
+
+${(props) => props.flow === 1 &&
       css`
+      border:2px dotted white;`}
+   
+`
 
-      box-shadow: 0.3em 0.3em 3em 0.2em hotpink;`}
-      `
+const MyGalaxy4 = styled.div`
+top:${props => props.distx}px;
+left:${props => props.disty}px;
+      
+text-align:center;
+position:absolute;
+display:flex;
+justify-content:center;
+align-items:center;
+background-image: linear-gradient(-20deg, #e9defa 0%, #fbfcdb 100%);;
+height: 50px;
+width: 50px;
+border-radius:50%;
+transform:translate(15px,15px);
+      
+${(props) => props.active === true &&
+    css`
+    box-shadow: 0.2em 0.2em 2em 0.2em gold;
+    border:2px dotted gold;`}
+      
+${(props) => props.focus === true &&
+    css`
+    box-shadow: 0.3em 0.3em 3em 0.2em hotpink;`}
 
-  const TestButton1 = styled.div`
+${(props) => props.flow === 1 &&
+     css`
+     border:2px dotted white;`}
+`
+
+const TestButton1 = styled.div`
     height:100px;
     width:100px;
     border:5px solid white;
@@ -193,7 +264,7 @@ css`
     justify-content:center;
     `
 
-  const TestButton2 = styled.div`
+const TestButton2 = styled.div`
     height:100px;
     width:100px;
     border:5px solid white;
@@ -207,7 +278,7 @@ css`
     justify-content:center;
     `
 
-    const TestButton3 = styled.div`
+const TestButton3 = styled.div`
     height:100px;
     width:100px;
     border:5px solid white;
@@ -221,7 +292,7 @@ css`
     justify-content:center;
     `
 
-  const TestButton4 = styled.div`
+const TestButton4 = styled.div`
     height:100px;
     width:100px;
     border:5px solid white;
--- utils/moving-functions.js
+++ utils/moving-functions.js
@@ -1,45 +1,118 @@
-// function downNow(){
+function turnRight(){
 
-//   const Base = galaxy[0] /// Objekt
+  const Base = galaxy[0] /// Objekt
   
-//   function hopDownInScope(object){
-
-//     const Current = object.children.find((element) => (element.active === true))
-//     const NewSubChildren = Current.children.map((element) => ({...element, focus: false}))
-//     const newChildren = object.children.map((element) => (element === Current? {...element, focus: true, active: false, children: NewSubChildren} : element))
-//     const newObject = {...object, children: newChildren, active:true}
-//     return newObject
-//      }
-
-//   if (Base.active === true){console.log("hier passiert nichts")}  
+  function turnFocusRightInScope(objekt){
+    const scopeX = objekt.children
+    const Focus = scopeX.find((element)=>(element.focus === true))
+    const nextFocusIndex = Focus.flow === 1 ? scopeX.length : Focus.flow - 1
+    const scopeXA = scopeX.map ((element)=>(element.focus === true ? {...element, focus:false} : element))
+    const scopeXNew = scopeXA.map ((element) => (element.flow === nextFocusIndex ? {...element, focus:true} : element))
+    return {...objekt, children:scopeXNew}
+} 
   
-//   else{
-    
-//   const subBase = Base.children.find((element)=>(element.scive === true))
-    
-//   if (subBase){
-//   const newGalaxy = [hopDownInScope(Base)]
-//   setGalaxy(newGalaxy)
-    
-//     } else {
+  if (Base.active === true){
+  
+  const newGalaxy = [turnFocusRightInScope(Base)]
+  setGalaxy(newGalaxy)
+  
+  } else {
+  
+  const subBaseOne = Base.children.find((element)=>(element.active === true)) // Objekt
+  const newSubBaseOne = turnFocusRightInScope(subBaseOne)
+  const NewScopeOne = Base.children.map((element)=>(element === subBaseOne ? newSubBaseOne : element))
+  const NewBase = {...Base, children: NewScopeOne}
+  setGalaxy([NewBase]) }
+  
+}
 
-// function findActiveCHild(OBJEKT){
-// const thereornot = OBJEKT.children.find((element)=>(element.active === true))
-// return thereornot
+function turnLeft(){
 
-// }
-    
-// const subBase2 = Base.children.find((element)=>(element.active === true)) // Objekt
+const Base = galaxy[0] /// Objekt
 
+function turnFocusLeftInScope(objekt){
+const scopeX = objekt.children
+const Focus = scopeX.find((element)=>(element.focus === true))
+const nextFocusIndex = Focus.flow === scopeX.length ? 1 : Focus.flow + 1
+const scopeXA = scopeX.map ((element)=>(element.focus === true ? {...element, focus:false} : element))
+const scopeXNew = scopeXA.map ((element) => (element.flow === nextFocusIndex ? {...element, focus:true} : element))
+return {...objekt, children:scopeXNew}}
 
+if (Base.active === true){
 
-//   const newSubBase = hopDownInScope(subBase)
-//   const NewScopeOne = Base.children.map((element)=>(element === subBase ? newSubBase : element))
-//   const NewBase = {...Base, children: NewScopeOne}
-//   setGalaxy([NewBase]) }}
+const newGalaxy = [turnFocusLeftInScope(Base)]
+setGalaxy(newGalaxy)
 
+} else {
 
+const subBaseOne = Base.children.find((element)=>(element.active === true)) // Objekt
+const newSubBaseOne = turnFocusLeftInScope(subBaseOne)
+const NewScopeOne = Base.children.map((element)=>(element === subBaseOne ? newSubBaseOne : element))
+const NewBase = {...Base, children: NewScopeOne}
+setGalaxy([NewBase])}
 
+}
 
-// }
+function upNow(){
 
+const Base = galaxy[0] /// Objekt
+
+function hopUpInScope(object){
+
+const Focus = object.children.find((element) => (element.focus === true))
+const NewSubChildren = Focus.children.map((element) => (element.flow === 1 ? {...element, focus: true} : element))
+const newChildren = object.children.map((element) => (element.focus === true ? {...element, focus: false, active: true, children: NewSubChildren} : element))
+const newObject = {...object, children: newChildren, active:false}
+return newObject
+}
+
+if (Base.active === true){
+
+const newGalaxy = [hopUpInScope(Base)]
+setGalaxy(newGalaxy)
+
+} else {
+
+const subBase = Base.children.find((element)=>(element.active === true)) // Objekt
+const newSubBase = hopUpInScope(subBase)
+const NewScopeOne = Base.children.map((element)=>(element === subBase ? newSubBase : element))
+const NewBase = {...Base, children: NewScopeOne}
+setGalaxy([NewBase])}
+}
+
+function downNow(){
+
+const Base = galaxy[0] /// Objekt
+
+function hopDownInScope(object){
+
+  const Current = object.children.find((element) => (element.active === true))
+  const NewSubChildren = Current.children.map((element) => ({...element, focus: false}))
+  const newChildren = object.children.map((element) => (element === Current? {...element, focus: true, active: false, children: NewSubChildren} : element))
+  const newObject = {...object, children: newChildren, active:true}
+  return newObject
+   }
+
+if (Base.active === true){console.log("hier passiert nichts")}  
+
+else{
+  
+const subBase = Base.children.find((element)=>(element.active === true))
+  
+if (subBase){
+const newGalaxy = [hopDownInScope(Base)]
+setGalaxy(newGalaxy)
+  
+  } else {
+
+function findActiveChild(objekt){
+const thereornot = objekt.children.find((element)=>(element.active === true))
+}
+  
+const subBase2 = Base.children.find((element)=>(findActiveChild(element) === true)) // Objekt
+const newSubBase2 = hopDownInScope(subBase2)
+const NewScopeOne = Base.children.map((element)=>(element === subBase2 ? newSubBase2 : element))
+const NewBase = {...Base, children: NewScopeOne}
+setGalaxy([NewBase]) }}
+
+}
